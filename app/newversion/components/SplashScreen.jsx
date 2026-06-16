5936b4e50ce1575e75f18f00f67862bc25a9dc15
Ayush Acharya <90086450+ayushacharya05@users.noreply.github.com>
2026-06-16 10:28:32 +0545

    Create SplashScreen.jsx

---
+++ app/newversion/components/SplashScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SplashScreen() {
+  return (
+    <div id="splashScreen">
+      <img src="https://raw.githubusercontent.com/ayushacharya05/aajakosamachar/main/images/icon.png" alt="Logo" />
+    </div>
+  );
+}
